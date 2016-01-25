5f638032a38a48c61eb01c994a37469ee67824a6
maoma87 <mao8a87@gmail.com>
2016-01-25 18:34:39 -0500

    inclusion de: gulp-uncss, gulp-autoprefixer, gulp-cssnano, gulp-uglify, gulp-rename

--- base-mao 2.0/gulpfile.js
+++ base-mao 2.0/gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
-var concat = require('gulp-concat');
+var uncss = require('gulp-uncss');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
 
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 
 // TASKS
@@ -13,12 +18,20 @@ gulp.task('jade', function() {
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest('/'))
 });
 
 gulp.task('sass', function () {
 	gulp.src('css/*.+(scss|sass)')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(uncss({
+        	html: ['*.html']
+        }))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(cssnano())
 		.pipe(gulp.dest('css'));
 });
 
@@ -28,22 +41,29 @@ gulp.task('concat', function() {
     .pipe(gulp.dest('js/'));
 });
 
-
-gulp.task('watch', function() {
-	//VIGILA LOS ARCHIVOS JADE DENTRO DE _includes/jade para compilar a html
-	gulp.watch('_includes/jade/**/*.jade', ['jade']);
-	//VIGILA LOS ARCHIVOS JADE DENTRO DE root para compilar a html
-	gulp.watch('*.jade', ['jade']);
-
-	//VIGILA LOS ARCHIVOS SASS DENTRO DE _includes/sass para compilar main.sass
-	gulp.watch('_includes/sass/**/*.+(scss|sass)', ['sass']);	
-
-
-	// Vigila los cambios en los archivos .js de los includes
-	//gulp.watch('_includes/js/*.js', ['concat']);
+gulp.task('compress', function() {
+	return gulp.src('js/functions.js')
+	.pipe(uglify())
+	.pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('js'));
 });
 
 
-// EJECUCION
+gulp.task('default', function() {
+	// VIGILA LOS ARCHIVOS JADE DENTRO DE _includes/jade para compilar a html
+	gulp.watch('_includes/jade/**/*.jade', ['jade']);
+	// VIGILA LOS ARCHIVOS JADE DENTRO DE root para compilar a html
+	gulp.watch('*.jade', ['jade']);
 
-gulp.task('default', ['watch']);
+	// VIGILA LOS ARCHIVOS SASS DENTRO DE _includes/sass para compilar main.sass
+	gulp.watch('_includes/sass/**/*.+(scss|sass)', ['sass']);
+	// VIGILA LOS ARCHIVOS SASS DENTRO DE css para compilar main.sass
+	gulp.watch('css/*.+(scss|sass)', ['sass']);
+
+	// Vigila los cambios en los archivos .js de los includes
+	gulp.watch('js/functions.js', ['concat']);
+	// Vigila los cambios en los archivos .js de los includes
+	//gulp.watch('_includes/js/*.js', ['concat']);
+});
