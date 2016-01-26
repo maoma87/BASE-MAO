6f31a792655c86c1a4208fe3f4725ffd819c3e69
maoma87 <mao8a87@gmail.com>
2016-01-26 14:44:33 -0500

    mejora de la arquitectura de archivos

--- base-mao 3.0/gulpfile.js
+++ base-mao 3.0/gulpfile.js
@@ -20,51 +20,51 @@ var rename = require("gulp-rename");
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
 
-  gulp.src('*.jade')
+  gulp.src('source/*.jade')
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
 	.pipe(prettify({indent_size: 4}))
-    .pipe(gulp.dest(''))
+    .pipe(gulp.dest('public'))
 });
 // FIN gulp-jade + gulp-prettify
 
 
 // gulp-sass + gulp-uncss + gulp-autoprefixer + gulp-cssnano
 gulp.task('sass', function () {
-	gulp.src('css/*.+(scss|sass)')
+	gulp.src('source/css/*.+(scss|sass)')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(uncss({
-        	html: ['*.html']
+        	html: ['public/*.html']
         }))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
 		.pipe(cssnano())
-		.pipe(gulp.dest('css'));
+		.pipe(gulp.dest('public/css'));
 });
 // FIN gulp-sass + gulp-uncss + gulp-autoprefixer + gulp-cssnano
 
 
 // gulp-concat
 gulp.task('concat', function() {
-	return gulp.src(['_includes/js/_jquery-2.2.0.min.js', '_includes/js/*.js'])
+	return gulp.src(['source/_includes/js/_jquery-2.2.0.min.js', 'source/_includes/js/*.js'])
 	.pipe(concat('scripts.js'))
 	.pipe(uglify())
-    .pipe(gulp.dest('js/'));
+    .pipe(gulp.dest('public/js/'));
 });
 // FIN gulp-concat
 
 
 // gulp-uglify + gulp-rename
 gulp.task('compress', function() {
-	return gulp.src('js/functions.js')
+	return gulp.src('source/js/functions.js')
 	.pipe(uglify())
-	.pipe(rename({
+	/*.pipe(rename({
       extname: '.min.js'
-    }))
-    .pipe(gulp.dest('js'));
+	}))*/
+    .pipe(gulp.dest('public/js'));
 });
 // FIN gulp-uglify + gulp-rename
 
@@ -72,13 +72,13 @@ gulp.task('compress', function() {
 // gulp-connect
 gulp.task('connect', function() {
   connect.server({
-    root: '',
+    root: 'public',
     livereload: true
   });
 });
 
 gulp.task('html', function () {
-  gulp.src('*.html')
+  gulp.src('public/*.html')
     .pipe(connect.reload());
 });
 // FIN gulp-connect
@@ -86,24 +86,24 @@ gulp.task('html', function () {
 // WATCH
 gulp.task('watch', function() {
 	// VIGILA LOS ARCHIVOS JADE DENTRO DE _includes/jade para compilar a html
-	gulp.watch('_includes/jade/**/*.jade', ['jade']);
+	gulp.watch('source/_includes/jade/**/*.jade', ['jade']);
 	// VIGILA LOS ARCHIVOS JADE DENTRO DE root para compilar a html
-	gulp.watch('*.jade', ['jade']);
+	gulp.watch('source/*.jade', ['jade']);
 
 	// VIGILA LOS ARCHIVOS SASS DENTRO DE _includes/sass para compilar main.sass
-	gulp.watch('_includes/sass/**/*.+(scss|sass)', ['sass']);
+	gulp.watch('source/_includes/sass/**/*.+(scss|sass)', ['sass']);
 	// VIGILA LOS ARCHIVOS SASS DENTRO DE css para compilar main.sass
-	gulp.watch('css/*.+(scss|sass)', ['sass']);
+	gulp.watch('source/css/*.+(scss|sass)', ['sass']);
 
 	// Vigila los cambios en los archivos .js de los includes
-	gulp.watch('js/functions.js', ['concat']);
+	gulp.watch('source/js/functions.js', ['compress']);
 	// Vigila los cambios en los archivos .js de los includes
 	//gulp.watch('_includes/js/*.js', ['concat']);
 
 	// VIGILA LOS ARCHIVOS HTML DENTRO DE root para live-sync
-	gulp.watch(['*.html'], ['html']);
+	gulp.watch(['public/*.html'], ['html']);
 	// VIGILA LOS ARCHIVOS CSS DENTRO DE CSS/ para live-sync
-	gulp.watch(['css/main.css'], ['html']);
+	gulp.watch(['public/css/main.css'], ['html']);
 });
 // FIN WATCH
 
