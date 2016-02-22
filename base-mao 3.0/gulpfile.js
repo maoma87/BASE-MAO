3f7b56dbc78a99c59d66d85d9f01e76473f66222
maoma87 <mao8a87@gmail.com>
2016-02-22 11:56:01 -0500

    mejoras varias
    mejora en la compilacion de sass con gulp
    adicion de otra fincion de gulp, sass-final para deploiment
    quitar configuaraciones de las tipografias en sass

--- base-mao 3.0/gulpfile.js
+++ base-mao 3.0/gulpfile.js
@@ -11,92 +11,145 @@ var sass = require('gulp-sass');
 var stripCssComments = require('gulp-strip-css-comments');
 var uncss = require('gulp-uncss');
 var autoprefixer = require('gulp-autoprefixer');
-var cssbeautify = require('gulp-cssbeautify');
+var csscomb = require('gulp-csscomb');
+var csso = require('gulp-csso');
 var cssnano = require('gulp-cssnano');
 
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
 
+
+// VARIABLES
 
 var carpeta = {
 	fuente: "source",
 	public: "public"
 };
 
+
 // TASKS ------------------------------------------------------------------
 
-// gulp-jade + gulp-prettify
+// COMPILAR JADE
 gulp.task('jade', function() {
 	var YOUR_LOCALS = {};
 
 	gulp.src(carpeta.fuente + '/*.jade')
+		// PREVIENE QUE LOS PROCESOS GULP.WATCH SE DETENGA AL ENCONTRAR UN ERROR
 		.pipe(plumber())
+
+		// COMPLIA JADE
 		.pipe(jade({
 			locals: YOUR_LOCALS
 		}))
+
+		// ENBELLECE EL HTML
 		.pipe(prettify({indent_size: 4}))
+
+		// GUARDA EL ARCHIVO HTML
 		.pipe(gulp.dest(carpeta.public))
+
+		// NOTIFICA QUE EL ARCHIVO .JADE SE COMPILO
 		.pipe( notify("JADE COMPILADO: <%= file.relative %>"))
+
 		// REFRESCADO DEL NAVEGADOR
 		.pipe(browserSync.stream());
 });
-// FIN gulp-jade + gulp-prettify
 
 
-// gulp-sass + gulp-uncss + gulp-autoprefixer + gulp-cssnano
+// COMPILAR SASS
 gulp.task('sass', function () {
 	gulp.src(carpeta.fuente + '/css/*.+(scss|sass)')
+		// PREVIENE QUE LOS PROCESOS GULP.WATCH SE DETENGA AL ENCONTRAR UN ERROR
 		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(stripCssComments({
-			//preserve: false
-		}))
-		// .pipe(autoprefixer({
-		// 	browsers: ['last 2 versions'],
-		// 	cascade: false
-		// }))
 
-		.pipe(cssnano())
-		.pipe(cssbeautify())
+		// TOMA LA INFORMACION PARA GENERAR EL MAPA DEL CSS
+		.pipe(sourcemaps.init())
+
+		// COMPILA SASS
+		.pipe(sass().on('error', sass.logError))
+
+		// GENERA EL MAPA DEL CSS
 		.pipe(sourcemaps.write('.'))
+
+		// GUARDA EL ARCHIVO CSS
 		.pipe(gulp.dest(carpeta.public + '/css'))
+
+		// GENERA NOTIFICACION AL COMPILAR EL SASS
 		.pipe( notify("SASS COMPILADO: <%= file.relative %>"))
+
 		// REFRESCADO DEL NAVEGADOR
 		.pipe(browserSync.stream());
 });
-// FIN gulp-sass + gulp-uncss + gulp-autoprefixer + gulp-cssnano
+
+// COMPILAR SASS
+gulp.task('sass-final', function () {
+	gulp.src(carpeta.fuente + '/css/*.+(scss|sass)')
+		// COMPILA SASS
+		.pipe(sass().on('error', sass.logError))
+
+		// AGREGA LACOMPATIBILIDAD CON TODOS LOS NAVEGADORES
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+
+		// OPTIMIZACION DEL CSS
+		.pipe(csso())
+
+		// MINIFICAR EL CSS
+		.pipe(cssnano())
+
+		// QUITA LOS COMENTARIOS DEL CSS
+		.pipe(stripCssComments({
+			preserve: false
+		}))
+
+		// GUARDA EL ARCHIVO CSS
+		.pipe(gulp.dest(carpeta.public + '/css'));
+});
 
 
-// gulp-uglify + gulp-rename
+// COMPRIME EL ARCHIVO DE FUNCIONES PRINCIPAL
 gulp.task('compress', function() {
 	return gulp.src(carpeta.fuente + '/js/functions.js')
+		// PREVIENE QUE LOS PROCESOS GULP.WATCH SE DETENGA AL ENCONTRAR UN ERROR
 		.pipe(plumber())
+
+		// COMPRIME EL JAVASCRIPT
 		.pipe(uglify())
-		/*.pipe(rename({
-			extname: '.min.js'
-		}))*/
+
+		// GUARDA EL ARCHIVO
 		.pipe(gulp.dest(carpeta.public + '/js'))
+
+		// NOTIFICA QUE EL ARCHIVO SE COMPRIMIO
 		.pipe( notify("FUNCIONES .JS COMPRIMIDO: <%= file.relative %>"))
+
 		// REFRESCADO DEL NAVEGADOR
 		.pipe(browserSync.stream());
 });
-// FIN gulp-uglify + gulp-rename
 
 
-// gulp-concat
+// CONCATENA Y COMPRIME LOS ARCHIVOS JS EN LA CARPETA JS DE INCLUDES
 gulp.task('concat', function() {
 	return gulp.src([carpeta.fuente + '/_includes/js/_jquery-2.2.0.min.js', carpeta.fuente + '/_includes/js/*.js'])
+		// PREVIENE QUE LOS PROCESOS GULP.WATCH SE DETENGA AL ENCONTRAR UN ERROR
 		.pipe(plumber())
+
+		// CONCATENA TODOS LOS ARCHIVOS JS DE LA CARPETA INCLUDES/JS Y NOMBRA EL NUEVO ARCHIVO
 		.pipe(concat('scripts.js'))
+
+		// COMPRIME EL JAVASCRIPT
 		.pipe(uglify())
+
+		// GUARDA EL ARCHIVO SCRIPTS.JS
 		.pipe(gulp.dest(carpeta.public + '/js/'))
+
+		// NOTIFICA QUE EL ARCHIVO SE CONCATENO
 		.pipe( notify("PLUG-INS .JS COMPRIMIDOS: <%= file.relative %>"))
+
 		// REFRESCADO DEL NAVEGADOR
 		.pipe(browserSync.stream());
 });
-// FIN gulp-concat
 
 
 // MONTAJE DEL SERVIDOR
@@ -107,7 +160,6 @@ gulp.task('servidor', function() {
 		notify: false
 	});
 });
-// FIN MONTAJE DEL SERVIDOR
 
 
 // WATCH
@@ -127,8 +179,6 @@ gulp.task('watch', function() {
 	// Vigila los cambios en los archivos .js de los includes
 	gulp.watch(carpeta.fuente + '/_includes/js/*.js', ['concat']);
 });
-// FIN WATCH
 
 // DEFAULT
 gulp.task('default', ['servidor', 'watch']);
-// FIN DEFAULT
