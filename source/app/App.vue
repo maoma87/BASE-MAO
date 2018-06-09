647ec4f250b33452f194990e5d12ac1d72b41d3a
Mauricio Santaella Ochoa <mao8a87@gmail.com>
2018-06-08 23:47:22 -0500

    vue router addition

--- source/app/App.vue
+++ source/app/App.vue
@@ -1,70 +1,31 @@
 <template>
 	<div id="app">
 
-		<!-- <router-view></router-view> -->
-
-		<vheader v-if="datos" :datos="datos.header"></vheader>
-
-		<vportfolio v-if="datos.portfolio" :datos="datos.portfolio"></vportfolio>
-		
-		<vabout v-if="datos.about" :datos="datos.about"></vabout>
-
-		<div class="mail-section orange" v-if="datos.about">
-			<a class="mail-link" href="#">{{datos.about.email}}</a>
-		</div>
-
-		<footer class="red footer" v-if="datos.header">
-			<span>{{datos.header.name}} {{new Date().getFullYear()}}</span>
-		</footer>
-
-		<vligthboxes v-if="datos.portfolio" :datos="datos.portfolio"></vligthboxes>
+		<router-view></router-view>
 
 	</div>
 </template>
 
 <script>
 
-import db from './firebase/init'
-// import axios from 'axios'
-import vheader 		from './components/Header.vue'
-import vportfolio 	from './components/Portfolio.vue'
-import vabout 		from './components/About.vue'
-import vligthboxes 	from './components/Ligthboxes.vue'
-
 export default {
 	name: 'app',
 
 	components: {
-		vheader,
-		vportfolio,
-		vabout,
-		vligthboxes
 	},
 
 	data(){
 		return{
-			datos:{}
 		}
 	},
 
 	methods:{
-		getRealTimeData() {
-			let app = this
-			let dbObject = {}
-			db.collection('web-data').onSnapshot(snapshot => {
-				snapshot.forEach(doc => {
-					dbObject[doc.id] = doc.data()
-				})
-				app.datos = dbObject
-			})
-		}
 	},
 	
 	computed:{
 	},
 
 	created(){
-		this.getRealTimeData()
 	},
 
 	mounted(){
