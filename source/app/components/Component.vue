aa92b201595b776feee3655f61856d74739d989f
Mao <mao8a87@gmail.com>
2019-05-14 09:27:04 -0500

    eslint small fixes

--- source/app/components/Component.vue
+++ source/app/components/Component.vue
@@ -10,8 +10,8 @@ export default {
 	name: "Vcomponent",
 	async mounted(){
 		// const call = await this.axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
-		// console.log(this.fb)
-
+		// console.log(call)
+		
 		// this.fb.db.collection("people").get()
 		// 	.then( querySnapshot => {
 		// 		console.log(querySnapshot)
